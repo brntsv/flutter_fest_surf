061a3f0ce0ae71d2ce7590d54858321c5c57f80b
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-28 19:14:42 +0700

    upd theme

--- lib/ui/themes/app_theme.dart
+++ lib/ui/themes/app_theme.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 abstract class AppTheme {
   static final light = ThemeData(
     primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.grey,
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.black,
+        unselectedItemColor: Color(0xFF52525E),
+        selectedItemColor: Color(0xFF00BD13)),
   );
 }
