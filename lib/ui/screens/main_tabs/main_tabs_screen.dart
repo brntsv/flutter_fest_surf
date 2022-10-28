9d2839e97d7bc5569e5ec3fca7565b966d06d9d4
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-29 01:06:12 +0700

    upd main tabs

--- lib/ui/screens/main_tabs/main_tabs_screen.dart
+++ lib/ui/screens/main_tabs/main_tabs_screen.dart
@@ -1,34 +1,99 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
+import 'package:flutter_fest_surf/resources/resources.dart';
+import 'package:flutter_fest_surf/ui/screens/main_tabs/main_tabs_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainTabsScreen extends StatelessWidget {
   const MainTabsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final currentIndex =
+        context.select((MainTabsViewModel vm) => vm.currentTabIndex);
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: Container()),
-          BottomNavigationBar(
-            currentIndex: 0,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.business),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.school),
-                label: '',
-              ),
-            ],
-            onTap: (_) {},
+      body: IndexedStack(
+        index: currentIndex,
+        children: const [
+          Center(
+            child: Text(
+              '1',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Center(
+            child: Text(
+              '2',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Center(
+            child: Text(
+              '3',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
+      bottomNavigationBar: const _BottomNavBarWidget(),
+    );
+  }
+}
+
+class _BottomNavBarWidget extends StatelessWidget {
+  const _BottomNavBarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final currentIndex =
+        context.select((MainTabsViewModel vm) => vm.currentTabIndex);
+    final model = context.read<MainTabsViewModel>();
+
+    final theme = Theme.of(context).bottomNavigationBarTheme;
+
+    final buttons = [
+      _BottomNavigationBarItemFactory(AppImages.tabbarCalendar),
+      _BottomNavigationBarItemFactory(AppImages.tabbarBookmark),
+      _BottomNavigationBarItemFactory(AppImages.tabbarPoint),
+    ]
+        .asMap()
+        .map((index, value) {
+          return MapEntry(index, value.build(index, currentIndex, theme));
+        })
+        .values
+        .toList();
+
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: model.setCurrentTabIndex,
+      items: buttons,
+    );
+  }
+}
+
+class _BottomNavigationBarItemFactory {
+  final String iconName;
+
+  _BottomNavigationBarItemFactory(this.iconName);
+
+  BottomNavigationBarItem build(
+    int index,
+    int currentIndex,
+    BottomNavigationBarThemeData theme,
+  ) {
+    final selectedColor = theme.selectedItemColor;
+    final unselectedColor = theme.unselectedItemColor;
+
+    final color = index == currentIndex ? selectedColor : unselectedColor;
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+        iconName,
+        color: color,
+      ),
+      label: '',
     );
   }
 }
