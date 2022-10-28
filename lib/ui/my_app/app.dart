323f26a33840c92ca0b89c42833352843062b318
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-28 17:18:09 +0700

    modify add

--- lib/ui/my_app/app.dart
+++ lib/ui/my_app/app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fest_surf/ui/navigation/main_navigation.dart';
 import 'package:flutter_fest_surf/ui/screens/timetable_screen.dart';
 import 'package:flutter_fest_surf/ui/themes/app_theme.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final mainNavigation = MainNavigation();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.light,
+      routes: mainNavigation.routes,
+      onGenerateRoute: mainNavigation.onGenerateRoute,
       home: const TimetableScreen(),
     );
   }
