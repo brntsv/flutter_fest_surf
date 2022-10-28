09a153d5138904d895c2d2f9ae4c430683729d65
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-28 17:51:01 +0700

    add main_tabs_screen and screen_factory

---
+++ lib/ui/screens/screen_factory/screen_factory.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fest_surf/ui/screens/main_tabs/main_tabs_screen.dart';
+
+class ScreenFactory {
+  Widget makeMainTabs() => const MainTabsScreen();
+}
