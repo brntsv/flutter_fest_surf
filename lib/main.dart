ddbef8f025fc2c0c2e546536f9461cebcb1d4eca
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-28 17:17:35 +0700

    modify main and test

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,6 @@ import 'package:flutter_fest_surf/ui/my_app/app.dart';
 import 'package:flutter_fest_surf/ui/screens/timetable_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  final app = MyApp();
+  runApp(app);
 }
