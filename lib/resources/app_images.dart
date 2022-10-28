8e130f2cab7498511403e6b5ef3b86bca027d4f2
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-29 01:08:39 +0700

    add resources

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,9 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String tabbarBookmark = 'assets/images/bookmark.png';
+  static const String tabbarCalendar = 'assets/images/tabbar_calendar.png';
+  static const String tabbarPoint = 'assets/images/point.png';
+}
