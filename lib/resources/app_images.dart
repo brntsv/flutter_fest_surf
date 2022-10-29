ba7c037b580ffe78e9ddbe3b65104928cbd3f930
Nikita Barantsev <nbarentsev@gmail.com>
2022-10-29 21:24:33 +0700

    add new images

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -3,6 +3,9 @@ part of 'resources.dart';
 class AppImages {
   AppImages._();
 
+  static const String toplogoGradient = 'assets/images/top_gradient.png';
+  static const String toplogoFlutterFest = 'assets/images/head.png';
+  static const String toplogoSurf = 'assets/images/surf.png';
   static const String tabbarBookmark = 'assets/images/bookmark.png';
   static const String tabbarCalendar = 'assets/images/tabbar_calendar.png';
   static const String tabbarPoint = 'assets/images/point.png';
