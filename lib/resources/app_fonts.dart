48e69955f332ea984460a33f57d25f8d11e80e62
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-01 01:36:56 +0700

    add fonts

---
+++ lib/resources/app_fonts.dart
@@ -0,0 +1,7 @@
+class AppFonts {
+  AppFonts._();
+
+  static const String basisGrotesqueProFont =
+      'assets/fonts/BasisGrotesquePro-Regular.ttf';
+  static const String steinbeckFont = 'assets/fonts/Steinbeck-Regular.otf';
+}
