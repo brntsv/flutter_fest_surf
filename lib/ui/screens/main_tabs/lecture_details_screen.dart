08e619588ade5f3c3ddb05a29ad25ec08f3854bc
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-05 16:43:10 +0700

    add info widgets

--- lib/ui/screens/main_tabs/lecture_details_screen.dart
+++ lib/ui/screens/main_tabs/lecture_details_screen.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:flutter_fest_surf/resources/resources.dart';
 import 'package:flutter_fest_surf/ui/themes/app_text_style.dart';
 import 'package:flutter_fest_surf/ui/themes/app_theme.dart';
@@ -133,7 +135,7 @@ class _DecriptionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 24),
       child: Text(
         'Текст описание краткое лекции. Что будет, раскрытие темы. Думаю, что на три или четыре строки. текста нет, так что пишу, что думаю бла бла бла...',
         style: AppTextStyle.bookText.copyWith(
@@ -149,8 +151,158 @@ class _ScheduleInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: const [
+          _ScheduleElementInfoWidget(
+            label: 'Старт',
+            info: '8:00',
+          ),
+          SizedBox(width: 16),
+          _ScheduleElementInfoWidget(
+            label: 'Секция',
+            info: '№ 1',
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class _ScheduleElementInfoWidget extends StatelessWidget {
+  final String label;
+  final String info;
+  const _ScheduleElementInfoWidget({
+    Key? key,
+    required this.label,
+    required this.info,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 76,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: AppColors.darkSecondary,
+      ),
+      child: GradientBorder(
+        strokeWidth: 2,
+        radius: 16,
+        gradient: const RadialGradient(
+          center: Alignment(0.1, -1),
+          radius: 1,
+          colors: [
+            Color(0xFF50AF64),
+            Color(0xFF3D734D),
+            Color(0xFF3D734D),
+            Color(0xFF206D82),
+            Color(0xFF0B77C5),
+          ],
+          stops: [
+            0.1,
+            0.7,
+            0.7,
+            0.9,
+            1,
+          ],
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 12, top: 10, right: 20, bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                label,
+                style:
+                    AppTextStyle.snackText.copyWith(color: AppColors.darkText),
+              ),
+              const SizedBox(height: 2),
+              Text(
+                info,
+                style: AppTextStyle.timeText.copyWith(color: AppColors.white88),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GradientBorder extends StatelessWidget {
+  final double strokeWidth;
+  final double radius;
+  final Gradient gradient;
+  final Widget child;
+  const GradientBorder({
+    Key? key,
+    required this.strokeWidth,
+    required this.radius,
+    required this.gradient,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+        painter: _GradientPainter(
+          strokeWidth: strokeWidth,
+          radius: radius,
+          gradient: gradient,
+        ),
+        child: child);
+  }
+}
+
+class _GradientPainter extends CustomPainter {
+  final double strokeWidth;
+
+  final double radius;
+  final Gradient gradient;
+
+  _GradientPainter({
+    required this.strokeWidth,
+    required this.radius,
+    required this.gradient,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // create outer rectangle equals size
+    final outerRect = Offset.zero & size;
+    final outerRRect = RRect.fromRectAndRadius(
+      outerRect,
+      Radius.circular(radius),
+    );
+
+    // create inner rectangle smaller by strokeWidth
+    final innerRect = Rect.fromLTWH(
+      strokeWidth,
+      strokeWidth,
+      size.width - strokeWidth * 2,
+      size.height - strokeWidth * 2,
+    );
+    final innerRRect = RRect.fromRectAndRadius(
+      innerRect,
+      Radius.circular(radius - strokeWidth),
+    );
+
+    // apply gradient shader
+    final paint = Paint()..shader = gradient.createShader(outerRect);
+
+    // create difference between outer and inner paths and draw it
+    final outerPath = Path()..addRRect(outerRRect);
+    final innerPath = Path()..addRRect(innerRRect);
+    final path = Path.combine(PathOperation.difference, outerPath, innerPath);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => oldDelegate != this;
 }
 
 class _AddToFavouriteButton extends StatelessWidget {
@@ -166,11 +318,11 @@ class _AddToFavouriteButton extends StatelessWidget {
         isFavourite ? AppColors.gradientOff : AppColors.gradientGreenBlue;
 
     return Padding(
-      padding: const EdgeInsets.only(left: 40, right: 40, bottom: 50),
+      padding: const EdgeInsets.only(left: 40, right: 40, bottom: 10),
       child: SizedBox(
         height: 48,
         child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 480),
+          constraints: const BoxConstraints(maxWidth: 300),
           child: ElevatedButton(
             onPressed: () {},
             style: ButtonStyle(
