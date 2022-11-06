9e20a0030da16ae538f7441c602976cb9703454d
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-06 20:56:44 +0700

    add easter bird

--- lib/ui/screens/main_tabs/lecture_details_screen.dart
+++ lib/ui/screens/main_tabs/lecture_details_screen.dart
@@ -16,12 +16,6 @@ class _LectureDetailsScreenState extends State<LectureDetailsScreen> {
   final _scrollController = ScrollController();
 
   @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -37,7 +31,7 @@ class _LectureDetailsScreenState extends State<LectureDetailsScreen> {
               left: 0,
               right: 0,
               bottom: 0,
-              child: Image.asset(AppImages.easterBirdLarge),
+              child: EasterBirdWidget(scrollController: _scrollController),
             ),
             ListView(
               padding: EdgeInsets.zero,
@@ -64,6 +58,73 @@ class _LectureDetailsScreenState extends State<LectureDetailsScreen> {
   void dispose() {
     _scrollController.dispose();
     super.dispose();
+  }
+}
+
+class EasterBirdWidget extends StatefulWidget {
+  final ScrollController scrollController;
+
+  const EasterBirdWidget({
+    Key? key,
+    required this.scrollController,
+  }) : super(key: key);
+
+  @override
+  State<EasterBirdWidget> createState() => _EasterBirdWidgetState();
+}
+
+class _EasterBirdWidgetState extends State<EasterBirdWidget> {
+  late ScrollController _scrollController;
+  var _easterBirdScale = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = widget.scrollController;
+
+    _scrollController.addListener(_onChangeScrollOffset);
+  }
+
+  @override
+  void didUpdateWidget(covariant EasterBirdWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (!identical(oldWidget.scrollController, widget.scrollController)) {
+      _scrollController.removeListener(_onChangeScrollOffset);
+      _scrollController = widget.scrollController;
+      _scrollController.addListener(_onChangeScrollOffset);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      scale: _easterBirdScale,
+      child: Image.asset(AppImages.easterBirdLarge),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onChangeScrollOffset);
+    super.dispose();
+  }
+
+  void _onChangeScrollOffset() {
+    // пояснения, эти строки не нужны для работы программы
+    _scrollController.offset;
+    // offset - это тот размер, на который контент проскроллен
+    _scrollController.position.maxScrollExtent;
+    // maxScrollExtent - это максимальная величина контента, на которую он
+    // может проскроллиться
+    final offset =
+        _scrollController.offset - _scrollController.position.maxScrollExtent;
+    // узнаем на сколько залезли за предел скролла
+    setState(() {
+      if (offset >= 0) {
+        _easterBirdScale = offset / 220;
+      }
+    });
   }
 }
 
