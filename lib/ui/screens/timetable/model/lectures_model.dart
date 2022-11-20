c3e08ba625ea6052f39758ab4701bdb772baed37
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-20 23:03:59 +0700

    upd model

--- lib/ui/screens/timetable/model/lectures_model.dart
+++ lib/ui/screens/timetable/model/lectures_model.dart
@@ -6,6 +6,7 @@ import 'package:flutter_fest_surf/ui/screens/timetable/widgets/schedule_row_widg
 
 List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
   ScheduleRowLectureWidgetConfiguration(
+    id: 1,
     avatarUrl:
         'https://i0.wp.com/sova.ponominalu.ru/wp-content/uploads/2018/04/post-malone.jpg?fit=800%2C600&ssl=1',
     speakerName: 'Алексей Чулпин',
@@ -15,6 +16,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 2,
     avatarUrl: 'https://tagline.ru/file/person/photo/2018/grigory-kochenov.png',
     speakerName: 'Гриша Коченов',
     lectureTitle: 'Непрошенная мораль про веб-дизайн',
@@ -23,6 +25,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 3,
     avatarUrl: 'https://greatheart.ru/assets/img/birthday/maksim-ponomarev.png',
     speakerName: 'Максим Понаморев',
     lectureTitle: 'Личный бренд в творческой профессии',
@@ -31,6 +34,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 4,
     avatarUrl:
         'https://hiphop4real.com/wp-content/uploads/2017/06/YUriy-Dud.jpg',
     speakerName: 'Юрий Дудь',
@@ -40,6 +44,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 5,
     avatarUrl:
         'https://pbs.twimg.com/profile_images/442246336553840640/tmlikWP-_400x400.jpeg',
     speakerName: 'Артём Малышев',
@@ -49,6 +54,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 6,
     avatarUrl:
         'https://images.ctfassets.net/9n3x4rtjlya6/47d55e95eURFdV4mlWzuur/ac7f0780a6ebba3f5cfa1a96fc54eaab/___________________.jpg',
     speakerName: 'Фил Ранжин',
@@ -58,6 +64,7 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   ScheduleRowLectureWidgetConfiguration(
+    id: 7,
     avatarUrl:
         'https://yt3.ggpht.com/ytc/AMLnZu89Ctj5mdefLspTmrKyy8VEBepwP38o8kBHvldVbg=s900-c-k-c0x00ffffff-no-rj',
     speakerName: 'Егор Малькевич',
@@ -70,36 +77,43 @@ List<ScheduleRowLectureWidgetConfiguration> listOfLectures = [
 
 List<ScheduleRowTimeWidgetConfiguration> listOfTime = [
   const ScheduleRowTimeWidgetConfiguration(
+    id: 1,
     startTime: '  8:00',
     endTime: '  9:30',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 2,
     startTime: '  9:35',
     endTime: '11:00',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 3,
     startTime: '11:30',
     endTime: '13:00',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 4,
     startTime: '13:05',
     endTime: '14:30',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 5,
     startTime: '15:00',
     endTime: '16:30',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 6,
     startTime: '16:35',
     endTime: '17:30',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
   ),
   const ScheduleRowTimeWidgetConfiguration(
+    id: 7,
     startTime: '18:00',
     endTime: '19:30',
     status: ScheduleRowWidgetConfigurationProgressStatus.coming,
@@ -128,27 +142,112 @@ class LecturesModel extends ChangeNotifier {
   List<ScheduleRowTimeWidgetConfiguration> get favouritesTime =>
       _favouritesTime;
 
-  /// метод добавления в избраное
-  void addToList(ScheduleRowLectureWidgetConfiguration item) {
-    _favourites.add(item);
+  /// метод добавления/удаления из избранного лекции
+  void toggleFavourite(ScheduleRowLectureWidgetConfiguration item) {
+    final isExist = _favourites.contains(item);
+    if (isExist) {
+      _favourites.remove(item);
+    } else {
+      _favourites.add(item);
+    }
+    _favourites.sort((a, b) => a.id.compareTo(b.id));
     notifyListeners();
   }
 
-  /// метод удаления из избраного
-  void removeFromList(ScheduleRowLectureWidgetConfiguration item) {
-    _favourites.remove(item);
+  /// метод такой же как выше, только для расписания
+  void toggleFavSchedule(ScheduleRowTimeWidgetConfiguration item) {
+    final isExist = _favouritesTime.contains(item);
+    if (isExist) {
+      _favouritesTime.remove(item);
+    } else {
+      _favouritesTime.add(item);
+    }
+    _favouritesTime.sort((a, b) => a.id.compareTo(b.id));
     notifyListeners();
   }
 
-  /// метод добавления в избраное для расписания
-  void addListTime(ScheduleRowTimeWidgetConfiguration item) {
-    _favouritesTime.add(item);
-    notifyListeners();
-  }
-
-  /// метод удаления из избраного для расписания
-  void removeListTime(ScheduleRowTimeWidgetConfiguration item) {
-    _favouritesTime.remove(item);
-    notifyListeners();
+  /// проверка для переключения иконки
+  bool isExists(ScheduleRowLectureWidgetConfiguration item) {
+    final isExist = _favourites.contains(item);
+    return isExist;
   }
 }
+
+class KeyLecture {
+  final int id;
+
+  KeyLecture(this.id);
+
+  @override
+  int get hashCode => id;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is KeyLecture && other.id == id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// метод добавления/удаления из избранного лекции
+  // void toggleFavourite(ScheduleRowLectureWidgetConfiguration item) {
+  //   final isExist = _favourites
+  //       .map((element) => element.speakerName)
+  //       .contains(item.speakerName);
+  //   if (isExist) {
+  //     _favourites.remove(item);
+  //   } else {
+  //     _favourites.add(item);
+  //   }
+  //   notifyListeners();
+  // }
+
+  // /// метод такой же как выше, только для расписания
+  // void toggleSchedule(ScheduleRowTimeWidgetConfiguration item) {
+  //   final isExist = _favouritesTime.contains(item);
+  //   if (isExist) {
+  //     _favouritesTime.remove(item);
+  //   } else {
+  //     _favouritesTime.add(item);
+  //   }
+  //   notifyListeners();
+  // }
+
+  // /// проверка для переключения иконки
+  // bool isExists(ScheduleRowLectureWidgetConfiguration item) {
+  //   final isExist = _favourites.contains(item);
+  //   return isExist;
+  // }
+
+  /// метод добавления в избраное
+  // void addToList(ScheduleRowLectureWidgetConfiguration item) {
+  //   _favourites.add(item);
+  //   notifyListeners();
+  // }
+
+  /// метод удаления из избраного
+  // void removeFromList(ScheduleRowLectureWidgetConfiguration item) {
+  //   _favourites.remove(item);
+  //   notifyListeners();
+  // }
+
+  /// метод добавления в избраное для расписания
+  // void addListTime(ScheduleRowTimeWidgetConfiguration item) {
+  //   _favouritesTime.add(item);
+  //   notifyListeners();
+  // }
+
+  /// метод удаления из избраного для расписания
+  // void removeListTime(ScheduleRowTimeWidgetConfiguration item) {
+  //   _favouritesTime.remove(item);
+  //   notifyListeners();
+  // }
